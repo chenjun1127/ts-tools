298d88c20dc559410ca417638c7e065d40d602d6
chenjun <chenjun@orvibo.com>
2023-04-19 16:37:50 +0800

    init

---
+++ src/camelize/index.ts
@@ -0,0 +1,7 @@
+const camelize = (str: string) => {
+  const camelizeRE = /-(\w)/g;
+  return str.replace(camelizeRE, function (_, c) {
+    return c ? c.toUpperCase() : '';
+  });
+};
+export default camelize;
